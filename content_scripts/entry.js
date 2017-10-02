7f373b822aef4bc029afe85882680980735e2f67
PrsPrsBK <prsprsbk@gmail.com>
2017-10-02 16:37:21 +0900

    can get and transform: overlay-tweet text

--- content_scripts/entry.js
+++ content_scripts/entry.js
@@ -14,19 +14,78 @@ function clickUpdateButton() {
   }
 }
 
+function copyTweet() {
+  let wk_elm;
+  wk_elm = document.getElementById('permalink-overlay-dialog');
+  if(wk_elm) {
+    copyOverlay(wk_elm);
+  }
+}
+
+function copyOverlay(tgt_elm) {
+  let result = {};
+  let wk_elm;
+  wk_elm = tgt_elm.getElementsByClassName('tweet-timestamp');
+  if(wk_elm && wk_elm.length > 0) {
+    result.href = wk_elm[0].href;
+    result.time = parseTweetTime(wk_elm[0].title.trim());
+  }
+  wk_elm = tgt_elm.getElementsByClassName('fullname');
+  if(wk_elm && wk_elm.length > 0) {
+    result.fullname = wk_elm[0].textContent.trim();
+  }
+  wk_elm = tgt_elm.getElementsByClassName('tweet-text');
+  if(wk_elm && wk_elm.length > 0) {
+    result.text = wk_elm[0].textContent.trim();
+  }
+  console.log(result);
+  let result_text = '<dt><a href="' +
+    result.href + '">' +
+    result.time['year'] + '-' +
+    result.time['month'] + '-' +
+    result.time['day'] + ' ' +
+    result.time['hour'] + ':' +
+    result.time['minute'] + ' ' +
+    result.fullname + ':</a>' +
+    result.text + '</dt>';
+  console.log(result_text);
+}
+
+function parseTweetTime(jp_str) {
+  //1:30 - 2017年9月30日
+  //data-time="1506188526" data-time-ms="1506188526000"
+  let wk = /([0-9]+):([0-9]+)[^0-9]+([0-9]+)年([0-9]+)月([0-9]+)日/.exec(jp_str);
+  let result = {};
+  result.year = wk[3];
+  result.month = ('00' + wk[4]).slice(-2);
+  result.day = ('00' + wk[5]).slice(-2);
+  result.hour = ('00' + wk[1]).slice(-2);
+  result.minute = ('00' + wk[2]).slice(-2);
+  return result;
+}
+
 function handleKeydown(ev) {
   var code = ev.keyCode;
   switch(code) {
     case 65: //0x41 A
-      console.log('A');
+      console.log(code + ':' + ev.ctrlKey);
       if(IS_AUTO_UPDATE === false) {
         INTERVAL_ID = setInterval(clickUpdateButton, CHECK_INTERVAL);
         browser.runtime.sendMessage({'badge':'on'});
         IS_AUTO_UPDATE = true;
       }
       break;
+    case 67: //0x43 C
+      console.log(code + ':' + ev.ctrlKey);
+      if(ev.ctrlKey) {
+        console.log(window.getSelection().toString());
+        if(window.getSelection().toString() === '') {
+          copyTweet();
+        }
+      }
+      break;
     case 81: //0x51 Q
-      console.log('Q');
+      console.log(code + ':' + ev.ctrlKey);
       if(IS_AUTO_UPDATE) {
         clearInterval(INTERVAL_ID);
         browser.runtime.sendMessage({'badge':'off'});
