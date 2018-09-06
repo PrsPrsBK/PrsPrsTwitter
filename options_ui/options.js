8f6a2481db0cd7697e62920eca1e1c2a2cc2cb72
PrsPrsBK <prsprsbk@gmail.com>
2018-09-07 05:14:16 +0900

    feat: prepare for option settings page

---
+++ options_ui/options.js
@@ -0,0 +1,6 @@
+if(typeof browser === 'undefined') {
+  window.browser = window.chrome;
+}
+
+
+// vim:expandtab ff=dos fenc=utf-8 sw=2
--- popup/popup.js
+++ popup/popup.js
@@ -1,2 +1,12 @@
-console.log('popup');
+if(typeof browser === 'undefined') {
+  window.browser = window.chrome;
+}
 
+document.addEventListener('click', (e) => {
+  if(e.target.id === 'go_addon_page') {
+    browser.runtime.openOptionsPage();
+  }
+  window.close();
+});
+
+// vim:expandtab ff=dos fenc=utf-8 sw=2
