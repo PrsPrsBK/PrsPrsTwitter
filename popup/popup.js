7052a2578a5db1b19c7169ff3496da0b79320bcd
PrsPrsBK <prsprsbk@gmail.com>
2018-09-08 19:38:55 +0900

    feat: popup: work for tweet list. show debug message

--- popup/popup.js
+++ popup/popup.js
@@ -2,6 +2,31 @@ if(typeof browser === 'undefined') {
   window.browser = window.chrome;
 }
 
+const popup = {
+  showDebugMessage : (message) => {
+    const tweetList = document.getElementById('tweet_list');
+    const wkTextNode = document.createTextNode(message);
+    const parElm = document.createElement('p');
+    parElm.appendChild(wkTextNode);
+    tweetList.appendChild(parElm);
+  },
+
+  loadTweets : () => {
+    const tweetList = document.getElementById('tweet_list');
+    while(tweetList.firstChild) {
+      tweetList.removeChild(tweetList.firstChild);
+    }
+    browser.tabs.query({currentWindow: true, active: true}, (tabs) => {
+      popup.showDebugMessage('can call query');
+      for(const tab of tabs) {
+        popup.showDebugMessage(`tab: ${tab.id}`);
+      }
+    });
+  },
+};
+
+document.addEventListener('DOMContentLoaded', popup.loadTweets);
+
 document.addEventListener('click', (e) => {
   if(e.target.id === 'go_addon_page') {
     browser.runtime.openOptionsPage();
