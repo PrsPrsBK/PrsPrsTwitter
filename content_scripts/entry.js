2f5a9303c8a2e89478854bb58e954c4a7295908c
PrsPrsBK <prsprsbk@gmail.com>
2018-09-08 20:13:06 +0900

    feat: disable mute and block by key with settings values

--- content_scripts/entry.js
+++ content_scripts/entry.js
@@ -1,11 +1,16 @@
-let IS_AUTO_UPDATE = true;
-let INTERVAL_ID;
-const CHECK_INTERVAL = 15000;
 
 if(typeof browser === 'undefined') {
   window.browser = window.chrome;
 }
 
+let INTERVAL_ID;
+// the same members as storage.local's
+let SETTINGS = {
+  update_check : true,
+  update_check_interval : 14000,
+  mute_by_key : false,
+  block_by_key : false,
+};
 const STORE_NAME = 'pptw_settings';
 
 const pptw = {
@@ -27,49 +32,27 @@ const pptw = {
   },
 
   handleKeydown : (evt) => {
-    if(evt.key === 'b') {
-      evt.preventDefault();
-    }
     if(evt.key === 'k') {
       console.log(`key ${evt.key}`);
       pptw.adjustScrollPos();
     }
-    else {
-      browser.storage.local.get(STORE_NAME, (store_obj) => {
-        const result = store_obj[STORE_NAME];
-        if(!result) {
-          return;
-        }
-        console.log(`${JSON.stringify(result,null,2)}`);
-        if(evt.key === 'b') {
-          if(!result.block_by_key) {
-            console.log(`key ${evt.key} res ${result.block_by_key}`);
-            // pptw.preventKeydown(evt);
-          }
-        }
-        else if(evt.key === 'u') {
-          if(!result.mute_by_key) {
-            console.log(`key ${evt.key} res ${result.block_by_key}`);
-            pptw.preventKeydown(evt);
-          }
-        }
-        else if(evt.key === 'a') {
-          IS_AUTO_UPDATE = result['update_check'];
-          if(IS_AUTO_UPDATE === false) {
-            INTERVAL_ID = setInterval(pptw.clickUpdateButton, CHECK_INTERVAL);
-            IS_AUTO_UPDATE = true;
-            pptw.updatePageAction();
-          }
-        }
-        else if(evt.key === 'q') {
-          IS_AUTO_UPDATE = result['update_check'];
-          if(IS_AUTO_UPDATE) {
-            clearInterval(INTERVAL_ID);
-            IS_AUTO_UPDATE = false;
-            pptw.updatePageAction();
-          }
-        }
-      });
+    else if(SETTINGS.block_by_key === false && evt.key === 'b') {
+      console.log(`block ${SETTINGS.block_by_key}`);
+      evt.preventDefault();
+    }
+    else if(SETTINGS.mute_by_key === false && evt.key === 'u') {
+      console.log(`mute ${SETTINGS.mute_by_key}`);
+      evt.preventDefault();
+    }
+    else if(SETTINGS.update_check === false && evt.key === 'a') {
+      INTERVAL_ID = setInterval(pptw.clickUpdateButton, SETTINGS.update_check_interval);
+      SETTINGS.update_check = true;
+      pptw.updatePageAction();
+    }
+    else if(SETTINGS.update_check && evt.key === 'q') {
+      clearInterval(INTERVAL_ID);
+      SETTINGS.update_check = false;
+      pptw.updatePageAction();
     }
   },
 
@@ -84,16 +67,24 @@ const pptw = {
   },
 
   updatePageAction : () => {
-    browser.runtime.sendMessage({task:'setIcon', icon: IS_AUTO_UPDATE});
+    browser.runtime.sendMessage({ task:'setIcon', icon: SETTINGS.update_check, });
   },
 
 };
 
 const start = () => {
-  console.debug(window.location.href);
-  pptw.updatePageAction();
-  document.addEventListener('keydown', pptw.handleKeydown);
-  INTERVAL_ID = setInterval(pptw.clickUpdateButton, CHECK_INTERVAL);
+  browser.storage.local.get(STORE_NAME, (store_obj) => {
+    const result = store_obj[STORE_NAME];
+    if(result) {
+      SETTINGS = result;
+    }
+    console.log(`${JSON.stringify(SETTINGS, null, 2)}`);
+    pptw.updatePageAction();
+    document.addEventListener('keydown', pptw.handleKeydown);
+    if(SETTINGS.update_check) {
+      INTERVAL_ID = setInterval(pptw.clickUpdateButton, SETTINGS.update_check_interval);
+    }
+  });
 };
 
 start();
