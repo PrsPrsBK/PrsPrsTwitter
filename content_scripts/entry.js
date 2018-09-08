8197a043db68bd1093af9ddc3fac72fcf78d8fdf
PrsPrsBK <prsprsbk@gmail.com>
2018-09-08 09:57:51 +0900

    refactor: content script

--- content_scripts/entry.js
+++ content_scripts/entry.js
@@ -6,73 +6,72 @@ if(typeof browser === 'undefined') {
   window.browser = window.chrome;
 }
 
-const updatePageAction = () => {
-  browser.runtime.sendMessage({task:'setIcon', icon: IS_AUTO_UPDATE});
-};
+const pptw = {
 
-/*
-clickUpdateButton():
-* click update button.
-*/
-const clickUpdateButton = () => {
-  const wk_elm = document.getElementsByClassName('new-tweets-bar');
-  if(wk_elm && wk_elm.length > 0) {
-    wk_elm[0].click();
-  }
-};
+  adjustScrollPos : () => {
+    //#page-container > div.ProfileCanopy.ProfileCanopy--withNav.ProfileCanopy--large.js-variableHeightTopBar > div > div.ProfileCanopy-header.u-bgUserColor
+    console.log('do');
+    const header = document.querySelector('#page-container > div.ProfileCanopy.ProfileCanopy--withNav.ProfileCanopy--large.js-variableHeightTopBar > div > div.ProfileCanopy-header.u-bgUserColor');
+    header.style.height = '280px';
+    const selectedItem = document.getElementsByClassName('selected-stream-item');
+    selectedItem[0].scrollIntoView();
+  },
+  
+  clickUpdateButton : () => {
+    const wk_elm = document.getElementsByClassName('new-tweets-bar');
+    if(wk_elm && wk_elm.length > 0) {
+      wk_elm[0].click();
+    }
+  },
+  
+  handleKeydown : (evt) => {
+    switch(evt.key) {
+      case 'a':
+        if(IS_AUTO_UPDATE === false) {
+          INTERVAL_ID = setInterval(pptw.clickUpdateButton, CHECK_INTERVAL);
+          IS_AUTO_UPDATE = true;
+          pptw.updatePageAction();
+        }
+        break;
+      case 'b':
+        pptw.preventKeydown(evt);
+        break;
+      case 'k':
+        pptw.adjustScrollPos();
+        break;
+      case 'q':
+        if(IS_AUTO_UPDATE) {
+          clearInterval(INTERVAL_ID);
+          IS_AUTO_UPDATE = false;
+          pptw.updatePageAction();
+        }
+        break;
+      case 'u':
+        pptw.preventKeydown(evt);
+        break;
+    }
+  },
+  
+  preventKeydown : (evt) => {
+    if(evt.target.isContentEditable
+      || evt.target.nodeName.toUpperCase() === 'INPUT'
+      || evt.target.nodeName.toUpperCase() === 'TEXTAREA') {
+      return;
+    }
+    evt.preventDefault();
+  },
 
-const preventKeydown = (evt) => {
-  if(evt.target.isContentEditable
-    || evt.target.nodeName.toUpperCase() === 'INPUT'
-    || evt.target.nodeName.toUpperCase() === 'TEXTAREA') {
-    return;
-  }
-  evt.preventDefault();
-};
+  updatePageAction : () => {
+    browser.runtime.sendMessage({task:'setIcon', icon: IS_AUTO_UPDATE});
+  },
 
-const adjustScrollPos = () => {
-  //#page-container > div.ProfileCanopy.ProfileCanopy--withNav.ProfileCanopy--large.js-variableHeightTopBar > div > div.ProfileCanopy-header.u-bgUserColor
-  console.log('do');
-  const header = document.querySelector('#page-container > div.ProfileCanopy.ProfileCanopy--withNav.ProfileCanopy--large.js-variableHeightTopBar > div > div.ProfileCanopy-header.u-bgUserColor');
-  header.style.height = '280px';
-  const selectedItem = document.getElementsByClassName('selected-stream-item');
-  selectedItem[0].scrollIntoView();
-};
-
-const handleKeydown = (evt) => {
-  switch(evt.key) {
-    case 'a':
-      if(IS_AUTO_UPDATE === false) {
-        INTERVAL_ID = setInterval(clickUpdateButton, CHECK_INTERVAL);
-        IS_AUTO_UPDATE = true;
-        updatePageAction();
-      }
-      break;
-    //case 'b':
-    //  preventKeydown(evt);
-    //  break;
-    case 'k':
-      adjustScrollPos();
-      break;
-    case 'q':
-      if(IS_AUTO_UPDATE) {
-        clearInterval(INTERVAL_ID);
-        IS_AUTO_UPDATE = false;
-        updatePageAction();
-      }
-      break;
-    //case 'u':
-    //  preventKeydown(evt);
-    //  break;
-  }
 };
 
 const start = () => {
   console.debug(window.location.href);
-  updatePageAction();
-  //DOMContentLoaded and load Event can be listend on capturing phase. not bubbling phase.
-  document.addEventListener('keydown', handleKeydown);
-  INTERVAL_ID = setInterval(clickUpdateButton, CHECK_INTERVAL);
+  pptw.updatePageAction();
+  document.addEventListener('keydown', pptw.handleKeydown);
+  INTERVAL_ID = setInterval(pptw.clickUpdateButton, CHECK_INTERVAL);
 };
 
 start();
