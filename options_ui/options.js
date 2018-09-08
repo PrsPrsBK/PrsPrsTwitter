42eb29094631ecb643a3c969b2ae5d5468356c0c
PrsPrsBK <prsprsbk@gmail.com>
2018-09-08 10:55:49 +0900

    feat: settings save and load

--- options_ui/options.js
+++ options_ui/options.js
@@ -2,9 +2,71 @@ if(typeof browser === 'undefined') {
   window.browser = window.chrome;
 }
 
-document.getElementById('save').addEventListener('click', (e) => {
-  console.log('save');
-});
+const STORE_NAME = 'pptw_settings';
+const SETTINGS_LIST = [
+  {
+    id: 'update_check',
+    value: 'checked',
+  },
+  {
+    id: 'update_check_interval',
+    value: 'value',
+  },
+  {
+    id: 'mute_by_key',
+    value: 'checked',
+  },
+  {
+    id: 'block_by_key',
+    value: 'checked',
+  },
+];
+
+const configUI = {
+
+  extractSettings : () => {
+    const ret = [];
+    SETTINGS_LIST.forEach((setting) => {
+      const inpElm = document.getElementById(setting.id);
+      ret.push({
+        id: setting.id,
+        value: setting.id === 'update_check_interval' ? parseInt(inpElm[setting.value]) : inpElm[setting.value],
+      });
+    });
+    console.log(`${JSON.stringify(ret)}`);
+    return ret;
+  },
+
+  restoreEntries : () => {
+    browser.storage.local.get(STORE_NAME, (store_obj) => {
+      const result = store_obj[STORE_NAME];
+      if(!result || !Array.isArray(result) || result.length === 0) {
+        console.log('nothing');
+        return;
+      }
+      console.log(`restore ${JSON.stringify(result)}`);
+      result.forEach((setting) => {
+        const inpElm = document.getElementById(setting.id);
+        if(typeof setting.value === 'boolean') {
+          inpElm.checked = setting.value;
+        }
+        else {
+          inpElm.value = setting.value;
+        }
+      });
+    });
+  },
+
+  saveEntries : () => {
+    browser.storage.local.set({
+      [STORE_NAME]: configUI.extractSettings(),
+    });
+  }
+
+};
+
+document.addEventListener('DOMContentLoaded', configUI.restoreEntries);
+document.getElementById('save').addEventListener('click', configUI.saveEntries);
 
 document.getElementById('update_check_interval').addEventListener("input", (e) => {
   if (e.target.validity.valid) {
