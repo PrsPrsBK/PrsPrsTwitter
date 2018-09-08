c44cd5faa98dbc017fc0ab2a624253ac47afe57e
PrsPrsBK <prsprsbk@gmail.com>
2018-09-08 09:39:16 +0900

    feat: UI: settings page

--- options_ui/options.js
+++ options_ui/options.js
@@ -2,5 +2,16 @@ if(typeof browser === 'undefined') {
   window.browser = window.chrome;
 }
 
+document.getElementById('save').addEventListener('click', (e) => {
+  console.log('save');
+});
+
+document.getElementById('update_check_interval').addEventListener("input", (e) => {
+  if (e.target.validity.valid) {
+    document.getElementById('save').disabled = false;
+  } else {
+    document.getElementById('save').disabled = true;
+  }
+});
 
 // vim:expandtab ff=dos fenc=utf-8 sw=2
