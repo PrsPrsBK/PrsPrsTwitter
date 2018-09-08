ac9b9bc61f3629c96537b9a1fb13e5bf01c37dfc
PrsPrsBK <prsprsbk@gmail.com>
2018-09-08 17:53:15 +0900

    feat: use values of setting. failed. preventDefault() is too late to have effect

--- content_scripts/entry.js
+++ content_scripts/entry.js
@@ -6,6 +6,8 @@ if(typeof browser === 'undefined') {
   window.browser = window.chrome;
 }
 
+const STORE_NAME = 'pptw_settings';
+
 const pptw = {
 
   adjustScrollPos : () => {
@@ -14,7 +16,7 @@ const pptw = {
     const header = document.querySelector('#page-container > div.ProfileCanopy.ProfileCanopy--withNav.ProfileCanopy--large.js-variableHeightTopBar > div > div.ProfileCanopy-header.u-bgUserColor');
     header.style.height = '280px';
     const selectedItem = document.getElementsByClassName('selected-stream-item');
-    selectedItem[0].scrollIntoView();
+    //selectedItem[0].scrollIntoView();
   },
   
   clickUpdateButton : () => {
@@ -23,41 +25,61 @@ const pptw = {
       wk_elm[0].click();
     }
   },
-  
+
   handleKeydown : (evt) => {
-    switch(evt.key) {
-      case 'a':
-        if(IS_AUTO_UPDATE === false) {
-          INTERVAL_ID = setInterval(pptw.clickUpdateButton, CHECK_INTERVAL);
-          IS_AUTO_UPDATE = true;
-          pptw.updatePageAction();
+    if(evt.key === 'b') {
+      evt.preventDefault();
+    }
+    if(evt.key === 'k') {
+      console.log(`key ${evt.key}`);
+      pptw.adjustScrollPos();
+    }
+    else {
+      browser.storage.local.get(STORE_NAME, (store_obj) => {
+        const result = store_obj[STORE_NAME];
+        if(!result) {
+          return;
         }
-        break;
-      case 'b':
-        pptw.preventKeydown(evt);
-        break;
-      case 'k':
-        pptw.adjustScrollPos();
-        break;
-      case 'q':
-        if(IS_AUTO_UPDATE) {
-          clearInterval(INTERVAL_ID);
-          IS_AUTO_UPDATE = false;
-          pptw.updatePageAction();
+        console.log(`${JSON.stringify(result,null,2)}`);
+        if(evt.key === 'b') {
+          if(!result.block_by_key) {
+            console.log(`key ${evt.key} res ${result.block_by_key}`);
+            // pptw.preventKeydown(evt);
+          }
         }
-        break;
-      case 'u':
-        pptw.preventKeydown(evt);
-        break;
+        else if(evt.key === 'u') {
+          if(!result.mute_by_key) {
+            console.log(`key ${evt.key} res ${result.block_by_key}`);
+            pptw.preventKeydown(evt);
+          }
+        }
+        else if(evt.key === 'a') {
+          IS_AUTO_UPDATE = result['update_check'];
+          if(IS_AUTO_UPDATE === false) {
+            INTERVAL_ID = setInterval(pptw.clickUpdateButton, CHECK_INTERVAL);
+            IS_AUTO_UPDATE = true;
+            pptw.updatePageAction();
+          }
+        }
+        else if(evt.key === 'q') {
+          IS_AUTO_UPDATE = result['update_check'];
+          if(IS_AUTO_UPDATE) {
+            clearInterval(INTERVAL_ID);
+            IS_AUTO_UPDATE = false;
+            pptw.updatePageAction();
+          }
+        }
+      });
     }
   },
-  
+
   preventKeydown : (evt) => {
     if(evt.target.isContentEditable
       || evt.target.nodeName.toUpperCase() === 'INPUT'
       || evt.target.nodeName.toUpperCase() === 'TEXTAREA') {
       return;
     }
+    console.log('preventkeydown');
     evt.preventDefault();
   },
 
