51960dd03dd707c56ea7905f3612af760d755a35
PrsPrsBK <prsprsbk@gmail.com>
2019-02-21 12:34:20 +0900

    feat: work around for damn and meaningless and braindead Google Chrome's  "default_popup"

---
+++ background/background_chrome.js
@@ -0,0 +1,23 @@
+if(typeof browser === 'undefined') {
+  window.browser = window.chrome;
+}
+
+/**
+ * Google Chrome's "default_popup" in manifest.json is truely meaningless suck,
+ * and requires such a ugly work around sucks.
+ */
+browser.tabs.onUpdated.addListener((tabId, chgInfo, _tab) => {
+  console.log(`chgInfo ${JSON.stringify(chgInfo)}`);
+  browser.pageAction.show(tabId);
+});
+
+browser.runtime.onMessage.addListener((message, sender) => {
+  if(message.task === 'setIcon') {
+    browser.pageAction.setIcon({
+      tabId: sender.tab.id,
+      path: message.icon ? 'icons/icon-run-48.png' : 'icons/icon-on.png',
+    });
+  }
+});
+
+// vim:expandtab ff=dos fenc=utf-8 sw=2
