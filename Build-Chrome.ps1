846b4ac1f918c7aa4cddacb0ea04aa069904c5b4
PrsPrsBK <prsprsbk@gmail.com>
2019-02-23 01:00:19 +0900

    feat: WIP: Chrome: manifest.json

--- Build-Chrome.ps1
+++ Build-Chrome.ps1
@@ -6,27 +6,20 @@ $manifest = Get-Content .\manifest.json | ConvertFrom-Json
       "strict_min_version": "64"
     }
   },
-  "options_ui": {
-    "open_in_tab": true,
-    "page": "options_ui/options.html"
-  },
   "page_action": {
     "browser_style": true,
-    "default_icon": {
-      "19": "icons/icon-on.png"
-    },
-    "default_popup": "popup/popup.html",
-    "default_title": "PrsPrsTwitter",
     "show_matches": [
       "https://twitter.com/*"
     ]
   },
 }
 #>
+$manifest | Add-Member minimum_chrome_version "72"
+$manifest | Add-Member short_name "PPTW"
 $manifest.PsObject.Members.Remove("applications")
 $pageAction = $manifest.page_action
 $pageAction | Add-Member chrome_style $pageAction.browser_style
 $pageAction.PsObject.Members.Remove("browser_style")
 $pageAction.PsObject.Members.Remove("show_matches")
 
-$manifest | ConvertTo-Json -Depth 9 | Out-File .\manifest_c.json
+$manifest | ConvertTo-Json -Depth 9 | Out-File .\chrome\manifest.json
